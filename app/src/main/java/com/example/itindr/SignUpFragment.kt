3b62b1533af5adc0ce370bae43e7f1bcb1d89530
The CNQRR <ap166884@gmail.com>
2026-03-02 13:34:27 +0700

    fix layout

--- app/src/main/java/com/example/itindr/SignUpFragment.kt
+++ app/src/main/java/com/example/itindr/SignUpFragment.kt
@@ -24,7 +24,7 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.signUnButton.setOnClickListener {
+        binding.signUpButton.setOnClickListener {
             findNavController().navigate(R.id.action_signUpFragment_to_aboutMeFragment)
         }
 
