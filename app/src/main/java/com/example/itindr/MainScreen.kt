f2191f147401ab29532d8bc8367604e0033f09d0
The CNQRR <ap166884@gmail.com>
2026-03-24 11:55:31 +0700

    fixed large bio size

--- app/src/main/java/com/example/itindr/MainScreen.kt
+++ app/src/main/java/com/example/itindr/MainScreen.kt
@@ -55,6 +55,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -284,6 +285,8 @@ fun PersonCard(
                         fontSize = 14.sp,
                         fontFamily = interRegular,
                         color = Color.White,
+                        maxLines = 20,
+                        overflow = TextOverflow.Ellipsis
                     )
                 }
             }
